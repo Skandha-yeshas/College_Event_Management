f9fbd8c450b8945661fbf207ca17e573b525d2b0
Yeshas <skandhayashas2906@gmail.com>
2025-10-31 17:07:20 +0530

    Update

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# Collect static files (optional but good practice)
+python manage.py collectstatic --noinput
